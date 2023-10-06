c37a99577c70379284d8c10d814aae4957f9b9a5
glasket <cws@glasket.com>
2023-10-05 22:22:22 -0400

    Add license info

--- src/result.c
+++ src/result.c
@@ -1,3 +1,18 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * @file result.c
+ * @brief Result type representing a value that can either be a value or an error.
+ * @author Christian Sigmon 'Glasket' <cws@glasket.com>
+ * @date 2023-
+ * @copyright MPL-2.0
+ * @version 0.1.0
+ */
+
 #include <assert.h>
 #include <cxl/result.h>
 
