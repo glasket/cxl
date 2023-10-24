67209c4000c197af03bfbe00e3a49ec9df6c546e
glasket <cws@glasket.com>
2023-10-24 01:56:53 -0400

    Add basic empty_init macro

---
+++ include/cxl/macros.h
@@ -0,0 +1,25 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * @file macros.h
+ * @brief General header for macros.
+ * @author Christian Sigmon 'Glasket' <cws@glasket.com>
+ * @date 2023-
+ * @copyright MPL-2.0
+ * @version 0.1.0
+ */
+
+#if __STDC_VERSION__ < 2023111L
+#define EMPTY_INIT(TYPE) \
+  (TYPE) { \
+    0 \
+  }
+#else
+#define EMPTY_INIT(TYPE) \
+  (TYPE) { \
+  }
+#endif
