d7ce4cd69fced04fbab7addf55cb1df975c9bd9d
glasket <cws@glasket.com>
2023-10-24 01:39:00 -0400

    Change name of result union member

--- include/cxl/result.h
+++ include/cxl/result.h
@@ -16,16 +16,13 @@
 #pragma once
 #ifndef CXL_RESULT_H
 #define CXL_RESULT_H
-
-#include "option.h"
-#include <stddef.h>
 #include <stdint.h>
 
 typedef struct XResult {
   union {
-    void *ptr;
+    void *ok;
     uintptr_t err; // uintptr_t so it aligns with void*
-  } res;
+  } value;
   bool is_err;
   bool is_ok;
 } XResult;
@@ -38,8 +35,5 @@ void *xres_unwrap_or(XResult res, void * or);
 void *xres_unwrap_or_else(XResult res, void *(*const f)(uintptr_t err));
 
 void *xres_unwrap_unchecked(XResult res);
-
-XOption xres_get_ok(XResult res);
-XOption xres_get_err(XResult res);
 
 #endif
--- src/result.c
+++ src/result.c
@@ -17,49 +17,35 @@
 #include <cxl/result.h>
 
 XResult xres_ok(void *ptr) {
-  return (XResult){.res.ptr = ptr, .is_err = false, .is_ok = true};
+  return (XResult){.value.ok = ptr, .is_err = false, .is_ok = true};
 }
 
 XResult xres_err(uintptr_t err) {
-  return (XResult){.res.err = err, .is_err = true, .is_ok = false};
+  return (XResult){.value.err = err, .is_err = true, .is_ok = false};
 }
 
 void *xres_unwrap(XResult res) {
   assert(res.is_ok);
   if (res.is_ok) {
-    return res.res.ptr;
+    return res.value.ok;
   }
   return nullptr;
 }
 
 void *xres_unwrap_or(XResult res, void *def) {
   if (res.is_ok) {
-    return res.res.ptr;
+    return res.value.ok;
   }
   return def;
 }
 
 void *xres_unwrap_or_else(XResult res, void *(*or_else)(uintptr_t err)) {
   if (res.is_ok) {
-    return res.res.ptr;
+    return res.value.ok;
   }
-  return or_else(res.res.err);
+  return or_else(res.value.err);
 }
 
 void *xres_unwrap_unchecked(XResult res) {
-  return res.res.ptr;
-}
-
-XOption xres_get_ok(XResult res) {
-  if (res.is_ok) {
-    return xopt_some(res.res.ptr);
-  }
-  return xopt_none();
-}
-
-XOption xres_get_err(XResult res) {
-  if (res.is_err) {
-    return xopt_some((void *)res.res.err);
-  }
-  return xopt_none();
+  return res.value.ok;
 }
