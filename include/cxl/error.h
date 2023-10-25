ec2d020df9b1f1ca6aa35eab125017e01bdb3004
glasket <cws@glasket.com>
2023-10-25 04:35:40 -0400

    Add header info to error.h
    - Add mismatched_alignment error

--- include/cxl/error.h
+++ include/cxl/error.h
@@ -1,3 +1,17 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * @file error.h
+ * @brief Error codes for CXL.
+ * @author Christian Sigmon 'Glasket' <cws@glasket.com>
+ * @date 2023-
+ * @copyright MPL-2.0
+ * @version 0.1.0
+ */
 
 #pragma once
 #ifndef CXL_ERROR_H
@@ -24,6 +38,7 @@ typedef enum : uintptr_t {
   X_ERR_BAD_INCREASE,
   X_ERR_BAD_SHRINK,
   X_ERR_BAD_OFFSET,
+  X_ERR_MISMATCHED_ALIGNMENT
 } XErr;
 
 bool xerr_is_ok(XErr err);
